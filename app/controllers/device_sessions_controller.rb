cbcf7d9dc8a5c99508b8543dbd4ea5e57b7268dd
kamil.sluszniak <sultanaghmed@gmail.com>
2018-11-24 16:05:27 +0100

    device sessions controller is storing device's ip

--- app/controllers/device_sessions_controller.rb
+++ app/controllers/device_sessions_controller.rb
@@ -8,8 +8,9 @@ class DeviceSessionsController < ApplicationController
     device = Device.find_by(name: name)
     if device && device.valid_password?(password)
       access_token = BCrypt::Password.create(password)
-      device.update_column(:authentication_token, access_token)
-       render json: {authentication_token: access_token}, status: 200
+      @current_ip = request.remote_ip
+      device.update_columns(authentication_token: access_token, last_sign_in_ip: @current_ip, current_sign_in_ip: @current_ip)
+      render json: {authentication_token: access_token}, status: 200
     else
       render json: "Invalid credentials", status: 401
     end
--- spec/controllers/device_sessions_controller_spec.rb
+++ spec/controllers/device_sessions_controller_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe DeviceSessionsController, type: :controller do
       expect(response).to have_http_status(401)
     end
 
-    it "returns http success on GET #new_session with credentials and provides a token" do
+    it "returns http success on GET #new_session with credentials and provides a token, sets ip columns" do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return('192.168.0.1')
       get :new_session, params: {:device => {:name => device.name, :password => "password"} }
 
+      expect(assigns(:current_ip)).to eq('192.168.0.1')
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body)).to have_key("authentication_token")
     end
