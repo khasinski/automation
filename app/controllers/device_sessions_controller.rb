b6567994753ea1ed5cc16f4c69ed905ad5619c7c
Kamil Sluszniak <kamil.sluszniak@gmail.com>
2020-06-09 11:42:21 +0200

    Migrate device_sessions_controller to hexagonal

--- app/controllers/device_sessions_controller.rb
+++ app/controllers/device_sessions_controller.rb
@@ -4,17 +4,13 @@ class DeviceSessionsController < ApplicationController
 
   def new_session
     return render json: "Invalid credentials", status: 401 unless params[:device]
-    name = device_params[:name]
-    password = device_params[:password]
-    device = Device.find_by(name: name)
-    if device && device.valid_password?(password)
-      access_token = BCrypt::Password.create(password)
-      @current_ip = request.remote_ip
-      device.update_columns(authentication_token: access_token, last_sign_in_ip: @current_ip, current_sign_in_ip: @current_ip)
-      render json: {authentication_token: access_token}, status: 200
-    else
-      render json: "Invalid credentials", status: 401
+    sign_in_device = SignInDevice.new
+    sign_in_device.on(:sign_in_device_success) do |device|
+      render json: {authentication_token: device.authentication_token}, status: 200
     end
+    sign_in_device.on(:sign_in_device_failed) { render json: "Invalid credentials", status: 401 }
+    @current_ip = request.remote_ip
+    sign_in_device.call(device_params, @current_ip)
   end
 
   private
---
+++ app/publishers/sign_in_device.rb
@@ -0,0 +1,17 @@
+class SignInDevice
+  include Wisper::Publisher
+
+  def call(sign_in_params, remote_ip)
+    name = sign_in_params[:name]
+    password = sign_in_params[:password]
+    device = Device.find_by(name: name)
+    if device && device.valid_password?(password)
+      access_token = BCrypt::Password.create(password)
+      current_ip = remote_ip
+      device.update_columns(authentication_token: access_token, last_sign_in_ip: current_ip, current_sign_in_ip: current_ip)
+      broadcast(:sign_in_device_success, device)
+    else
+      broadcast(:sign_in_device_failed)
+    end
+  end
+end
